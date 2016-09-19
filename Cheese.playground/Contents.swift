56a312790438d811dd4fdb9071ce0d05405c0291
Greg Lhotellier <glhotellier@gmail.com>
2016-09-19 13:03:59 +0200

    Rename Form -> Cheese

---
+++ Cheese.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+import PlaygroundSupport
+
+let root = CheesesListController()
+let navigationController = UINavigationController(rootViewController: root)
+
+PlaygroundPage.current.liveView = navigationController
+
