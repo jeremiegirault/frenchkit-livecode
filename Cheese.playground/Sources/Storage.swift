a60c71088655e833e8df21f64ccc943f8646986e
greg3z <glhotellier@gmail.com>
2016-09-20 11:31:51 +0200

    Stink update

--- Cheese.playground/Sources/Storage.swift
+++ Cheese.playground/Sources/Storage.swift
@@ -8,23 +8,23 @@ public class CheeseStorage {
     private let duration: Double = 0.6
     private var cheeses: [Cheese] = [
         Cheese(name: "Abondance", stinks: false, imageName: "abondance"),
-        Cheese(name: "Emmental", stinks: true, imageName: "allgauer-emmentaler"),
         Cheese(name: "Banon", stinks: true, imageName: "banon"),
         Cheese(name: "Bleu d'Auvergne", stinks: true, imageName: "bleu-dauvergne"),
         Cheese(name: "Bleu de Gex", stinks: true, imageName: "bleu-de-gex"),
-        Cheese(name: "Brie de Meaux", stinks: true, imageName: "brie-de-meaux"),
-        Cheese(name: "Brie", stinks: true, imageName: "brie"),
+        Cheese(name: "Brie de Meaux", stinks: false, imageName: "brie-de-meaux"),
+        Cheese(name: "Brie", stinks: true, imageName: "brie")
     ]
     
     public var availableCheeses: [Cheese] = [
         Cheese(name: "Brillat Savarin", stinks: true, imageName: "brillat-savarin"),
-        Cheese(name: "Brocciu", stinks: true, imageName: "brocciu"),
+        Cheese(name: "Brocciu", stinks: false, imageName: "brocciu"),
         Cheese(name: "Bucheron", stinks: true, imageName: "bucheron"),
-        Cheese(name: "Cabrales", stinks: true, imageName: "cabrales"),
+        Cheese(name: "Cabrales", stinks: false, imageName: "cabrales"),
         Cheese(name: "Camembert", stinks: true, imageName: "camembert"),
-        Cheese(name: "Cantal", stinks: true, imageName: "cantal"),
+        Cheese(name: "Cantal", stinks: false, imageName: "cantal"),
         Cheese(name: "Chabichou du Poitou", stinks: true, imageName: "chabichou-du-poitou"),
         Cheese(name: "Chaource", stinks: true, imageName: "chaource"),
+        Cheese(name: "Emmental", stinks: false, imageName: "allgauer-emmentaler")
     ]
     
     private func modelDidChange() {
