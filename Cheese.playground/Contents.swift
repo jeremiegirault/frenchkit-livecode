57338892ce170c10dafca57e92761dc1e7e6b9db
greg3z <glhotellier@gmail.com>
2016-09-20 11:39:28 +0200

    Router class

--- Cheese.playground/Contents.swift
+++ Cheese.playground/Contents.swift
@@ -1,18 +1,26 @@
 import UIKit
 import PlaygroundSupport
 
-let root = CheesesListController()
-let navigationController = UINavigationController(rootViewController: root)
 
-root.title = "Cheeses List"
-let addCheeseButton = UIBarButtonItem(barButtonSystemItem: .add, target: root, action: #selector(CheesesListController.addCheese))
-root.navigationItem.rightBarButtonItem = addCheeseButton
+final class Router {
+    
+    let rootViewController: UIViewController
+    
+    init() {
+        let root = CheesesListController()
+        let navigationController = UINavigationController(rootViewController: root)
 
-root.cheeseTouched = {
-    let cheeseDetailsController = CheeseDetailsController(cheese: $0)
-    cheeseDetailsController.title = $0.name
-    navigationController.pushViewController(cheeseDetailsController, animated: true)
+        root.title = "Cheeses List"
+        let addCheeseButton = UIBarButtonItem(barButtonSystemItem: .add, target: root, action: #selector(CheesesListController.addCheese))
+        root.navigationItem.rightBarButtonItem = addCheeseButton
+
+        root.cheeseTouched = {
+            let cheeseDetailsController = CheeseDetailsController(cheese: $0)
+            cheeseDetailsController.title = $0.name
+            navigationController.pushViewController(cheeseDetailsController, animated: true)
+        }
+        rootViewController = navigationController
+    }
 }
 
-PlaygroundPage.current.liveView = navigationController
-
+PlaygroundPage.current.liveView = Router().rootViewController
